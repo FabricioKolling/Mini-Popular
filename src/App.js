027dd457458d327b1f3cfbb89932963c64585f4e
Fabricio Kolling <162700727+FabricioKolling@users.noreply.github.com>
2025-07-08 10:07:20 -0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-
 import NavbarMenu from './Pages/NavbarMenu';
+import Footer from './Components/Footer';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <NavbarMenu/>
+      {/* NOVO: Container para o resto do conteúdo da sua página */}
+      <main className="main-content">
+        {/*
+          Aqui virá o resto do seu site:
+          - Banners
+          - Seção de produtos
+          - Rodapé
+          - etc.
+        */}
+        <h1>Conteúdo da Página Começa Aqui</h1>
+      </main>
+      <footer>
+        <Footer/>
+      </footer>
     </div>
   );
 }
--- src/Pages/NavbarMenu.js
+++ src/Pages/NavbarMenu.js
@@ -1,23 +1,76 @@
+import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import React from 'react'
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import Logo from '../Img/logo2.png';
+
+import { FaSearch, FaMicrophone, FaUserCircle, FaFilm } from 'react-icons/fa';
+import { BsFillBookmarkFill } from 'react-icons/bs';
 
 function NavbarMenu() {
   return (
     <>
-      <Navbar className='navbar'>
+      <Navbar bg="light" expand="lg" className="top-navbar">
+        <Container fluid className="d-flex justify-content-center">
+
+          <div className="navbar-content-wrapper d-flex align-items-center justify-content-between w-100">
+
+            <Navbar.Brand href="#home" className="p-0">
+              <img
+                src={Logo}
+                className="logo"
+                alt="Mini Popular Logo"
+              />
+            </Navbar.Brand>
+
+            <div className="search-container flex-grow-1 mx-4">
+              <FaSearch className="search-icon" />
+              <Form.Control
+                type="search"
+                placeholder="Pesquise aqui..."
+                className="search-input"
+                aria-label="Search"
+              />
+              <FaMicrophone className="mic-icon" />
+            </div>
+
+            <Nav className="d-flex flex-row align-items-center">
+              <Nav.Link href="#login" className="d-flex align-items-center me-3 login-section">
+                <FaUserCircle size={35} className="login-icon" />
+                <div className="login-text ms-2">
+                  <span>Entre ou</span>
+                  <span>Cadastre-se</span>
+                </div>
+              </Nav.Link>
+
+              <Button variant="success" className="cart-button me-3">
+                <BsFillBookmarkFill size={20} />
+                <span className="ms-2">0</span> 
+              </Button>
+
+              <Nav.Link href="#roleta">
+                <FaFilm size={40} className="roulette-icon" />
+              </Nav.Link>
+            </Nav>
+
+          </div>
+        </Container>
+      </Navbar>
+
+      <Navbar className="custom-navbar" variant="dark">
         <Container>
-          <Nav className="ms-auto">
-            <Nav.Link href="#home" className='menu'>Home</Nav.Link>
-            <Nav.Link href="#Sobre" className='menu'>Mini Popular</Nav.Link>
-            <Nav.Link href="#produtos" className='menu'>Produtos</Nav.Link>
-            <Nav.Link href="#contato" className='menus'>Contato</Nav.Link>
+          <Nav className="mx-auto">
+            <Nav.Link href="#home" className='menu-item'>Home</Nav.Link>
+            <Nav.Link href="#Sobre" className='menu-item'>Mini Popular</Nav.Link>
+            <Nav.Link href="#produtos" className='menu-item'>Produtos</Nav.Link>
+            <Nav.Link href="#contato" className='menu-item'>Contato</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
-      </>
+    </>
   );
 }
 
-export default NavbarMenu
+export default NavbarMenu;
